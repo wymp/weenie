98dfa66808916a963661a0d405a5c8c1d94397db
Kael Shipman <kael.shipman@cfxtrading.com>
2020-04-23 22:11:35 -0500

    Added tests for config

--- tests/Config.spec.ts
+++ tests/Config.spec.ts
@@ -1,5 +1,22 @@
-import { configFromEnv } from "../src/Config";
+import { configFromFiles, configFromEnv } from "../src/Config";
 import * as rt from "runtypes";
+import * as fs from "fs";
+
+// Stub out file reader
+let ConfigFileContents: string = "{}";
+let ConfigOverrideContents: string = "{}";
+let ConfigOverrideExists = true;
+let ConfigFileNum: number = 1;
+(fs.existsSync as any) = (f: string) => ConfigOverrideExists;
+(fs.readFileSync as any) = (f: string) => {
+  if (ConfigFileNum === 1) {
+    ConfigFileNum++;
+    return ConfigFileContents;
+  } else {
+    ConfigFileNum--;
+    return ConfigOverrideContents;
+  }
+};
 
 const testConfigValidator = rt.Record({
   a: rt.Record({
@@ -10,15 +27,61 @@ const testConfigValidator = rt.Record({
   b: rt.Boolean.Or(rt.Undefined).Or(rt.Null),
   c: rt.Literal("Exactly C"),
 });
+declare type TestConfig = rt.Static<typeof testConfigValidator>;
 
 describe("Config functions", () => {
+  describe("configFromFile", () => {
+    const par =
+      "NOTE: YOU MUST USE THE -i FLAG TO TELL JEST NOT TO RUN IN PARALLEL FOR THESE TESTS";
+    beforeEach(() => {
+      ConfigFileContents = JSON.stringify({
+        a: {
+          one: 1,
+          two: "Two",
+          three: true,
+        },
+        b: false,
+        c: "Exactly C",
+      });
+      ConfigOverrideContents = "{}";
+      ConfigOverrideExists = true;
+    });
+
+    test("Loads config from files", () => {
+      const r = configFromFiles<TestConfig>("test", "test", testConfigValidator)();
+      expect(r.config).toBeDefined();
+      expect(r.config.a.one).toBe(1);
+      expect(r.config.b).toBe(false);
+      //expect(r.config.four).not.toBeDefined(); // < This should fail type-checking on uncomment
+    });
+
+    test("Can override default config", () => {
+      ConfigOverrideContents = JSON.stringify({ a: { two: "Three" }, b: true });
+      const r = configFromFiles<TestConfig>("test", "test", testConfigValidator)();
+      expect(r.config).toBeDefined();
+      expect(r.config.a.one).toBe(1);
+      expect({ par, v: r.config.a.two }).toEqual({ par, v: "Three" });
+      expect(r.config.b).toBe(true);
+    });
+
+    test("Skips override if file not exists", () => {
+      ConfigOverrideContents = JSON.stringify({ a: { two: "Three" }, b: true });
+      ConfigOverrideExists = false;
+      const r = configFromFiles<TestConfig>("test", "test", testConfigValidator)();
+      expect(r.config).toBeDefined();
+      expect(r.config.a.one).toBe(1);
+      expect({ par, v: r.config.a.two }).toEqual({ par, v: "Two" });
+      expect(r.config.b).toBe(false);
+    });
+  });
+
   describe("configFromEnv", () => {
     beforeEach(() => {
-      delete process.env.APP_a_one
-      delete process.env.APP_a_two
-      delete process.env.APP_a_three
-      delete process.env.APP_b
-      delete process.env.APP_c
+      delete process.env.APP_a_one;
+      delete process.env.APP_a_two;
+      delete process.env.APP_a_three;
+      delete process.env.APP_b;
+      delete process.env.APP_c;
     });
 
     function setEnv() {
@@ -30,12 +93,12 @@ describe("Config functions", () => {
     }
 
     [
-      [ "APP_a_one", "3", "a.one" ],
-      [ "APP_a_two", "12345", "a.two" ],
-      [ "APP_a_three", "string", "a.three" ],
-      [ "APP_b", "string", "b" ],
-      [ "APP_c", "Not C", "c" ],
-    ].map((testCase) => {
+      ["APP_a_one", "3", "a.one"],
+      ["APP_a_two", "12345", "a.two"],
+      ["APP_a_three", "string", "a.three"],
+      ["APP_b", "string", "b"],
+      ["APP_c", "Not C", "c"],
+    ].map(testCase => {
       test(`should throw errors when ${testCase[0]} is ${testCase[1]}`, () => {
         // Set the config in the environment
         setEnv();
@@ -59,10 +122,10 @@ describe("Config functions", () => {
     });
 
     [
-      [ "APP_b", null ],
-      [ "APP_b", "null" ],
-      [ "APP_a_three", "false" ],
-    ].map((testCase) => {
+      ["APP_b", null],
+      ["APP_b", "null"],
+      ["APP_a_three", "false"],
+    ].map(testCase => {
       test(`should successfully draw config from environment`, () => {
         setEnv();
         if (testCase[1] === null) {
@@ -70,7 +133,9 @@ describe("Config functions", () => {
         } else {
           process.env[<string>testCase[0]] = testCase[1];
         }
-        const { config } = configFromEnv<rt.Static<typeof testConfigValidator>>(testConfigValidator)();
+        const { config } = configFromEnv<rt.Static<typeof testConfigValidator>>(
+          testConfigValidator
+        )();
         expect(config.a.one).toBe(1);
         expect(config.a.two).toBe("Two");
         expect(config.a.three).toBe(false);
